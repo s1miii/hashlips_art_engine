c159edf4d97e8ee3a99b5233840876dfca7e6bbe
Simi <84782994+s1miii@users.noreply.github.com>
2023-08-19 11:11:34 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -5,12 +5,12 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "Your Collection";
-const description = "Remember to replace this description";
+const namePrefix = "Developer Base";
+const description = "Developer NFTs only limited 222 collection.";
 const baseUri = "ipfs://NewUriToReplace";
 
 const solanaMetadata = {
-  symbol: "YC",
+  symbol: "DEV",
   seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
   external_url: "https://www.youtube.com/c/hashlipsnft",
   creators: [
@@ -26,13 +26,13 @@ const layerConfigurations = [
   {
     growEditionSizeTo: 5,
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Based" },
+      { name: "Vibe" },
+      { name: "Clothing" },
+      { name: "Os" },
+      { name: "Flag" },
+      { name: "Computer" },
+      
     ],
   },
 ];
